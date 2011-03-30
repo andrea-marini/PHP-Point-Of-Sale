9fd3c5e00ac22b8e732c0390c3f1fc8d98f6a702
Chris Muench <me@chrismuench.com>
2011-03-30 19:55:04 -0400

    Fixed database constraint issue

--- database/database_11.0-isaac.sql
+++ database/database_11.0-isaac.sql
@@ -2,3 +2,5 @@ ALTER TABLE  `phppos_customers` ADD  `zone` VARCHAR( 255 ) NOT NULL AFTER  `taxa
 ALTER TABLE  `phppos_sales` ADD  `delivery_date` DATE NOT NULL AFTER  `sale_time` ,
 ADD  `delivery_time` VARCHAR( 255 ) NOT NULL AFTER  `delivery_date`;
 ALTER TABLE  `phppos_sales` ADD  `balance` DOUBLE( 15, 2 ) NOT NULL AFTER  `delivery_time`;
+ALTER TABLE phppos_sales_items_taxes DROP FOREIGN KEY phppos_sales_items_taxes_ibfk_2;
+ALTER TABLE `phppos_sales_items_taxes` ADD CONSTRAINT `phppos_sales_items_taxes_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `phppos_items` (`item_id`);
