a98486f7a4e00142c746dd7ebce0aee0d230e2ed
Chris Muench <me@chrismuench.com>
2011-04-03 11:59:32 -0400

    Move detailed reports to the top

--- application/views/reports/listing.php
+++ application/views/reports/listing.php
@@ -3,6 +3,20 @@
 <div id="page_title" style="margin-bottom:8px;"><?php echo $this->lang->line('reports_reports'); ?></div>
 <div id="welcome_message"><?php echo $this->lang->line('reports_welcome_message'); ?>
 <ul id="report_list">
+
+	<li><h3><?php echo $this->lang->line('reports_detailed_reports'); ?></h3>
+		<ul>
+			<li><a href="<?php echo site_url('reports/delivery');?>"><?php echo $this->lang->line('reports_delivery'); ?></a></li>
+			<li><a href="<?php echo site_url('reports/driver');?>"><?php echo $this->lang->line('reports_driver'); ?></a></li>
+			<li><a href="<?php echo site_url('reports/debt');?>"><?php echo $this->lang->line('reports_debt_report'); ?></a></li>
+			<li><a href="<?php echo site_url('reports/detailed_sales');?>"><?php echo $this->lang->line('reports_sales'); ?></a></li>
+			<li><a href="<?php echo site_url('reports/detailed_receivings');?>"><?php echo $this->lang->line('reports_receivings'); ?></a></li>
+			<li><a href="<?php echo site_url('reports/specific_customer');?>"><?php echo $this->lang->line('reports_customer'); ?></a></li>
+			<li><a href="<?php echo site_url('reports/specific_employee');?>"><?php echo $this->lang->line('reports_employee'); ?></a></li>
+		</ul>
+	
+	</li>
+
 	<li><h3><?php echo $this->lang->line('reports_graphical_reports'); ?></h3>
 		<ul>
 			<li><a href="<?php echo site_url('reports/graphical_summary_sales');?>"><?php echo $this->lang->line('reports_sales'); ?></a></li>
@@ -29,21 +43,7 @@
 			<li><a href="<?php echo site_url('reports/summary_discounts');?>"><?php echo $this->lang->line('reports_discounts'); ?></a></li>
 			<li><a href="<?php echo site_url('reports/summary_payments');?>"><?php echo $this->lang->line('reports_payments'); ?></a></li>
 		</ul>
-	</li>
-	
-	<li><h3><?php echo $this->lang->line('reports_detailed_reports'); ?></h3>
-		<ul>
-			<li><a href="<?php echo site_url('reports/delivery');?>"><?php echo $this->lang->line('reports_delivery'); ?></a></li>
-			<li><a href="<?php echo site_url('reports/driver');?>"><?php echo $this->lang->line('reports_driver'); ?></a></li>
-			<li><a href="<?php echo site_url('reports/debt');?>"><?php echo $this->lang->line('reports_debt_report'); ?></a></li>
-			<li><a href="<?php echo site_url('reports/detailed_sales');?>"><?php echo $this->lang->line('reports_sales'); ?></a></li>
-			<li><a href="<?php echo site_url('reports/detailed_receivings');?>"><?php echo $this->lang->line('reports_receivings'); ?></a></li>
-			<li><a href="<?php echo site_url('reports/specific_customer');?>"><?php echo $this->lang->line('reports_customer'); ?></a></li>
-			<li><a href="<?php echo site_url('reports/specific_employee');?>"><?php echo $this->lang->line('reports_employee'); ?></a></li>
-		</ul>
-	
-	</li>
-	
+	</li>	
 	<li><h3><?php echo $this->lang->line('reports_inventory_reports'); ?></h3>
 		<ul>
 			<li><a href="<?php echo site_url('reports/inventory_low');?>"><?php echo $this->lang->line('reports_low_inventory'); ?></a></li>
